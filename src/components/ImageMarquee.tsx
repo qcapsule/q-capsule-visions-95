35e0ff788f169c6b4931a4ffe749c01f296c3ef4
Aly Soliman <alysoliman05@gmail.com>
2025-10-04 17:45:30 +0300

    fix image marquee

--- src/components/ImageMarquee.tsx
+++ src/components/ImageMarquee.tsx
@@ -59,7 +59,7 @@ const ImageMarqueeRow = memo(({ images, speed = 40 }: ImageMarqueeRowProps) => {
         className="flex gap-4 w-max"
       >
         {[...images, ...images].map((image, index) => (
-          <img
+          <div
             key={index}
             className={`relative ${image.aspectRatio} h-80 rounded-2xl overflow-hidden shadow-lg`}
           >
